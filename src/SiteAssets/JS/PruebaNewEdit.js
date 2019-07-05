5571469520da21eb31db45c5f09a2af899e6ee1b
Juan <jponce@mstech.la>
2019-07-05 10:50:57 -0300

    05.07.19-10.50

--- src/SiteAssets/JS/PruebaNewEdit.js
+++ src/SiteAssets/JS/PruebaNewEdit.js
@@ -1,12 +1,12 @@
 ﻿var itemId;
 var source;
 
-$(document).ready(function(){
-	JSRequest.EnsureSetup();
+$(document).ready(function () {
+    JSRequest.EnsureSetup();
     itemId = JSRequest.QueryString["ID"];
     source = JSRequest.QueryString["Source"];
-    if(source == undefined)
-		source = "/";
+    if (source == undefined)
+        source = "/";
 
     swal({
         title: 'Cargando...',
@@ -14,47 +14,48 @@ $(document).ready(function(){
             swal.showLoading();
         }
     });
-    
+
     SP.SOD.executeFunc('sp.js', 'SP.ClientContext', Iniciar);
 });
 
-function Iniciar(){
-	CargarDetalle();
+function Iniciar() {
+    CargarDetalle();
 }
 
-function CargarDetalle(){
-	var url = location.origin + "/_api/web/currentUser";
+function CargarDetalle() {
+    var url = location.origin + "/_api/web/currentUser";
     $.ajax({
-		method: "GET",
-		url: url,
-		headers: {"Accept": "application/json;odata=verbose"}
-	}).then(function(data){
-		if(data.d.results != null){
-			for(i = 0; i < data.d.results.length; i++){
-	       		var option = $('<option>').val(data.d.results[i].ID).text(data.d.results[i].Title);
-	       		
-				$('#ddlDetalle').append($(option));
-	      	}
-	      	
-			swal.close();
-		}
-		else{
-			swal('Error', 'Se produjo un error al recuperar el detalle', 'error');
-		}
-	});
+        method: "GET",
+        url: url,
+        headers: { "Accept": "application/json;odata=verbose" }
+    }).then(function (data) {
+        if (data.d != null) {
+                var option = $('<option>').val(data.d.Title).text(data.d.Title);
+
+                $('#ddlDetalle').append($(option));
+            
+            swal.close();
+        }
+        else {
+            swal('Error', 'Se produjo un error al recuperar el detalle', 'error');
+        }
+    },
+        function (error) {
+            swal('Error', 'Se produjo un error al recuperar el detalle', 'error');
+        });
 }
 
-function Guardar(){
-	$('#divBotonera').hide();
-	
-	var item = {
+function Guardar() {
+    $('#divBotonera').hide();
+
+    var item = {
         "__metadata": { "type": "SP.Data.PruebaListItem" },
-        "Title"    		: $('#inTitulo').val(),
-        "Fecha"			: $('#inFecha').val(),
-        "DetalleId"  	: $('#ddlDetalle').val(),
-        "Activo"     	: $('#chkActivo').prop('checked')
+        "Title": $('#inTitulo').val(),
+        "Fecha": $('#inFecha').val(),
+        "DetalleId": $('#ddlDetalle').val(),
+        "Activo": $('#chkActivo').prop('checked')
     }
-    
+
     var url = "/_api/web/lists/getByTitle('Prueba')/items";
     $.ajax({
         url: url,
@@ -65,16 +66,16 @@ function Guardar(){
             "Accept": "application/json;odata=verbose",
             "X-RequestDigest": $("#__REQUESTDIGEST").val(),
         },
-        success: function(result) {
+        success: function (result) {
             swal({
-            	title: 'Éxito!',
-            	text: 'Se guardó correctamente',
-            	type: 'success'
-            }).then(function(){
-            	this.location.href = decodeURIComponent(source);
+                title: 'Éxito!',
+                text: 'Se guardó correctamente',
+                type: 'success'
+            }).then(function () {
+                this.location.href = decodeURIComponent(source);
             });
         },
-        error: function(error){
+        error: function (error) {
             swal('Se produjo un error al guardar', error.responseJSON.error.message.value, 'error');
             $('#divBotonera').show();
             return false;
@@ -82,6 +83,6 @@ function Guardar(){
     });
 }
 
-function Cancelar(){
+function Cancelar() {
     this.location.href = decodeURIComponent(source);
 }
